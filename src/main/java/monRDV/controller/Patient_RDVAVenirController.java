9d7a196d0f0ef1ddb2d26247bbc0ca3cfc153d73
Gonzales <arthur.af.gonzales@gmail.com>
2018-11-30 17:15:40 +0100

    arthur

--- src/main/java/monRDV/controller/Patient_RDVAVenirController.java
+++ src/main/java/monRDV/controller/Patient_RDVAVenirController.java
@@ -1,7 +1,6 @@
 package monRDV.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,9 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import monRDV.model.CreneauDisponible;
-import monRDV.model.Patient;
 import monRDV.model.RendezVous;
-import monRDV.model.Utilisateur;
 import monRDV.repository.IRepositoryCreneauDisponible;
 import monRDV.repository.IRepositoryPatient;
 import monRDV.repository.IRepositoryRendezVous;
@@ -56,14 +53,6 @@ public class Patient_RDVAVenirController {
 		model.addAttribute("rdvAVenirCreneaux", creneaux);
 		
 		
-//		Optional<Utilisateur> optUtilisateur = repoUtilisateur.findById(id);
-//		if (optUtilisateur.isPresent()) {
-//			Utilisateur utilisateur = optUtilisateur.get();
-//		}
-//		List<Patient> patients = repoPatient.findByUtilisateur(id);
-//		model.addAttribute("page", "eleve");
-//		model.addAttribute("rdvAVenirPatients", patients); // ETAPE 3
-
 		return "patient/patientRDVAVenir"; // ETAPE 4
 	}
 
