b1a7f15a381df3db613da3cca70b23ea81e47f31
Gonzales <arthur.af.gonzales@gmail.com>
2018-11-30 11:47:45 +0100

    arthur

---
+++ src/main/java/monRDV/controller/Patient_RDVAVenirController.java
@@ -0,0 +1,59 @@
+package monRDV.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import monRDV.model.Patient;
+import monRDV.model.Utilisateur;
+import monRDV.repository.IRepositoryPatient;
+import monRDV.repository.IRepositoryRendezVous;
+import monRDV.repository.IRepositoryUtilisateur;
+
+@Controller
+@RequestMapping("/monComptePatient")
+public class Patient_RDVAVenirController {
+
+	@Autowired
+	private IRepositoryUtilisateur repoUtilisateur;
+
+	@Autowired
+	private IRepositoryPatient repoPatient;
+
+	@Autowired
+	private IRepositoryRendezVous repoRendezVous;
+
+	public Patient_RDVAVenirController() {
+		super();
+
+	}
+	
+	@GetMapping({ "/", "/rdvAVenir" }) // ETAPE 1
+	public String listvide() {
+
+		return "patient/patientRDVAVenir"; // ETAPE 4
+	}
+
+	@GetMapping({ "/yo", "/rdvAVenir" }) // ETAPE 1
+	public String list(@RequestParam Long id ,Model model) {
+		Optional<Utilisateur> optUtilisateur = repoUtilisateur.findById(id);
+		
+		if (optUtilisateur.isPresent()) {
+			Utilisateur utilisateur= optUtilisateur.get();
+		}
+		List<Patient> patients =  repoPatient.findByUtilisateur(id);
+
+//		model.addAttribute("page", "eleve");
+		
+		model.addAttribute("rdvAVenirPatients", patients); // ETAPE 3
+
+		return "patient/patientRDVAVenir"; // ETAPE 4
+	}
+
+}
--- src/main/java/monRDV/repository/IRepositoryPatient.java
+++ src/main/java/monRDV/repository/IRepositoryPatient.java
@@ -1,9 +1,16 @@
 package monRDV.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import monRDV.model.Patient;
 
 public interface IRepositoryPatient extends JpaRepository<Patient, Long>{
+
+	@Query("select p from Patient p left join fetch p.utilisateur where p.id = :id")
+	List<Patient> findByUtilisateur(@Param("id") Long identifiantUtilisateur);
 
 }
