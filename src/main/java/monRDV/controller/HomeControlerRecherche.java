8c8336b9acf2cdade3c7ebac8a0000627e85843b
charlotte mouden <charlotte.mouden@live.fr>
2018-11-30 14:14:37 +0100

    Home update

--- src/main/java/monRDV/controller/HomeControlerRecherche.java
+++ src/main/java/monRDV/controller/HomeControlerRecherche.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -34,8 +35,9 @@ public class HomeControlerRecherche {
 		return "home/homerecherche"; // ETAPE 4
 	}
 	
-
+	@PostMapping("/listByVille")
 	public String listByVille(Model model, @RequestParam String ville) { 
+		
 		List<Praticien> praticiens = repoPraticien.findByVille(ville); 
 
 		model.addAttribute("page", "accueil");
