1a3286903520f514f8f853223edfc9221e48e823
vjorda <virginie.jorda@gmail.com>
2018-11-30 12:53:53 +0100

    home vivi

--- src/main/java/monRDV/controller/HomeController.java
+++ src/main/java/monRDV/controller/HomeController.java
@@ -1,5 +1,29 @@
 package monRDV.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import monRDV.repository.IRepositorySpecialite;
+
+@Controller
 public class HomeController {
+
+	@Autowired
+	private IRepositorySpecialite repoSpecialite;
+
+	public HomeController() {
+		super();
+	}
+
+	@GetMapping("")
+	public String home(Model model) {
+
+		model.addAttribute("page", "home");
+		model.addAttribute("specialites", repoSpecialite.findAll()); // ETAPE 3
+
+		return "home/home";
+	}
 
 }
