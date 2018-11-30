601c57b45c6056ca08f64191fa1f69b4f386ae44
vjorda <virginie.jorda@gmail.com>
2018-11-30 14:47:21 +0100

    home

--- src/main/java/monRDV/repository/IRepositoryPraticien.java
+++ src/main/java/monRDV/repository/IRepositoryPraticien.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import monRDV.model.Praticien;
 
 public interface IRepositoryPraticien extends JpaRepository<Praticien, Long>{
+	
 	@Query("select p from Praticien p left join fetch p.lieux l where l.adresse.ville = :ville")
 	List<Praticien> findByVille(@Param("ville") String ville);
 }
