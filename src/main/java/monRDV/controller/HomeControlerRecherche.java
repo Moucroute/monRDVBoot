81499762eef20202bbdc78e8b15e10bb59c72d5c
SimonV <simon.varvat@gmail.com>
2018-12-01 15:23:49 +0100

    push toi d'la que j'm'y mette !

--- src/main/java/monRDV/controller/HomeControlerRecherche.java
+++ src/main/java/monRDV/controller/HomeControlerRecherche.java
@@ -1,10 +1,8 @@
 package monRDV.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
